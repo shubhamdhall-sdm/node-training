744b590b759b80c5060db8a52449d4d2b936fd58
Shubham Dhall <77053124+shubhamdhall-sdm@users.noreply.github.com>
2024-09-19 10:13:09 +0530

    Update fileUploader.js

--- fileUploader.js
+++ fileUploader.js
@@ -60,6 +60,88 @@ exports.multerUploadMiddleware = (multerUploadFunction) => {
         });
 }
 
+const multer = require('multer');
+const fs = require("fs");
+const { petPictureMaxFileSize } = require('../../storage/fileSize');
+const { v4: uuidv4 } = require('uuid');
+const { petDocuments } = require('../../../files-path/pet/path');
+
+/*
+ * pet files storage configuration
+*/
+let petFilesStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        let dir = petDocuments;
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, {
+                recursive: true
+            });
+        }
+        cb(null, dir);
+    },
+    filename: (req, file, cb) => {
+        cb(null, uuidv4() + file.originalname);
+    }
+});
+
+
+/*
+ * pet files uploader configuration
+*/
+let petFilesUploader = multer({
+    storage: petFilesStorage,
+    fileFilter: (req, file, cb) => {
+        if (file.fieldname == 'pet_image') {
+            if (file.mimetype == 'image/png' || file.mimetype == 'image/jpg' || file.mimetype == 'image/jpeg') {
+                cb(null, true);
+            }
+            else {
+                cb(null, false);
+                req.fileValidationError = {
+                    fieldname: file.fieldname,
+                    error: 'Only .png, .jpg and .jpeg type formats are allowed'
+                }
+                return cb(new Error('Only .png, .jpg and .jpeg type formats are allowed'));
+            }
+        } else {
+            if (
+                file.mimetype == 'image/png' ||
+                file.mimetype == 'image/jpg' ||
+                file.mimetype == 'image/jpeg' || 
+                file.mimetype == 'application/pdf' ||
+                file.mimetype == 'application/vnd.ms-excel' ||
+                file.mimetype == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' ||
+                file.mimetype == 'text/csv' ||
+                file.mimetype == 'application/msword' ||
+                file.mimetype == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+            ) {
+                cb(null, true);
+            }
+            else {
+                cb(null, false);
+                req.fileValidationError = {
+                    fieldname: file.fieldname,
+                    error: 'Only .png, .jpg, .jpeg, .pdf, .xls, .xlsx, .csv, .doc and .docx file formats are allowed'
+                }
+                return cb(new Error('Only .png, .jpg, .jpeg, .pdf, .xls, .xlsx, .csv, .doc and .docx file formats are allowed'));
+            }
+            // cb(null, false);
+            // req.fileValidationError = {
+            //     fieldname: file.fieldname,
+            //     error: 'This field name is not allowed.'
+            // };
+            // return cb(new Error('This field name is not allowed.'));
+        }
+    },
+    limits: { fileSize: petPictureMaxFileSize }
+});
+
+
+/*
+ * pet files upload image configuration
+*/
+exports.uploadPetFilesImage = petFilesUploader.any(); 
+
 let forClinic;
             let petImage = '';
             let petDocuments = [];
