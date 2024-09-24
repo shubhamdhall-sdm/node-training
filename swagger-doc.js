09aa9c301b9e504326d1a9d09722317eb6f08428
Shubham Dhall <77053124+shubhamdhall-sdm@users.noreply.github.com>
2024-09-24 10:16:27 +0530

    Create swagger-doc.js

---
+++ swagger-doc.js
@@ -0,0 +1,39 @@
+import { Router } from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
+
+export const apiDoc = (app: Router) => {
+  const options = {
+    swaggerDefinition: {
+      swagger: "2.0",
+      info: {
+        title: "project API Documentation",
+        version: "0.0.2",
+      },
+      host: `${process.env.BASEURL}`,
+      basePath: "/",
+      securityDefinitions: {
+        basicAuth: {
+          type: 'basic',
+          name: 'authorization',
+        },
+        Key: {
+          type: 'apiKey',
+          in: 'query',
+          name: 'api_key'
+        }
+      },
+      tags: [
+        {
+          name: 'Authentication',
+        }
+      ],
+      schemes: ["http", "https"],
+    },
+    apis: ["doc/*.yaml"],
+  };
+  const swaggerSpec = swaggerJSDoc(options);
+  app.use("/project/documentation", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+};
+
+apiDoc(app);
