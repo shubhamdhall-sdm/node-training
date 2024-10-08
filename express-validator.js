9828e7440c33eeeb8fcdad521fa4b031ab075acd
Shubham Dhall <77053124+shubhamdhall-sdm@users.noreply.github.com>
2024-10-08 18:03:11 +0530

    Update express-validator.js

--- express-validator.js
+++ express-validator.js
@@ -72,9 +72,6 @@ const { validationResult } = require("express-validator");
 exports.validationResult = (req, res, next) => {
   try {
     validationResult(req).throw();
-    if (req.body.email) {
-      req.body.email = req.body.email.toLowerCase();
-    }
     return next();
   } catch (err) {
     return error;
