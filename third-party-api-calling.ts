37843d466534f8485a593fc11846cfffe6e7c6fc
Shubham Dhall <77053124+shubhamdhall-sdm@users.noreply.github.com>
2024-10-21 11:24:02 +0530

    Update third-party-api-calling.ts

--- third-party-api-calling.ts
+++ third-party-api-calling.ts
@@ -1,13 +1,14 @@
-async fileData(type: string, name: string, owner?: string, parent?: string, subjectOrg?: string, hidden?: boolean){
+let path = process.ENV.SERVICE_PATH
+
+async fileData(type: string, name: string, owner?: string, parent?: string, hidden?: boolean){
     let json = {
       type: type,
-      originalName: name,
+      name: name,
       owner: owner || '',
       parent: parent || '',
-      subjectOrg: subjectOrg,
       hidden: hidden || false
     }
-    const response = await fetch(`${path}/file-services`, {
+    const response = await fetch(`${path}/file`, {
       method: 'POST', 
       body: JSON.stringify(json),
       headers: {'Content-Type': 'application/json'}
@@ -16,3 +17,35 @@ async fileData(type: string, name: string, owner?: string, parent?: string, subj
     console.log(body)
     return body
   }
+
+async getDoc(id: string){
+    const response = await fetch(`${path}/file/fileGet/${id}`, {
+      method: 'GET', 
+      headers: {'Content-Type': 'application/json'}
+    });
+    const body = await response.json();
+    return body
+  }
+
+  async deleteDoc(id: string, authorization?: any){
+
+    const response = await fetch(`${path}/file/${id}`, {
+      method: 'DELETE', 
+      headers: {'Content-Type': 'application/json', 'Authorization': authorization || ''}
+    });
+    const body = await response.json();
+    return body
+  }
+
+  async updateDoc(id: string, name: string) {
+    let json = {
+        name: name,
+    }
+    const response = await fetch(`${path}/file/name/${id}`, {
+        method: 'PATCH',
+        body: JSON.stringify(json),
+        headers: { 'Content-Type': 'application/json' }
+    });
+    const body = await response.json();
+    return body
+}
